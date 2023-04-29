8f0f6349396b6a7000b44e98909a81a593f75f58
Vahit Bugra YESILKAYNAK <bugrayesilkaynak@gmail.com>
2023-04-29 02:12:58 +0200

    6 yıllık birtakım hatalar düzeltirdi, güle güle kullanın

--- gunes_sistemi.py
+++ gunes_sistemi.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 yer_cekimi_sabiti = 6.67408 * 1.e-11  # metre^3 / (kilogram * saniye^2)
 
@@ -28,9 +29,10 @@ merkur_gunes_uzaklik = 5.791 * 1.e10
 ay_gunes_uzaklik = dunya_gunes_uzaklik + 3.844 * 1.e5
 saturn_gunes_uzaklik = 1.429 * 1.e12
 
-ayrik_zaman = 100000  # saniye
+ayrik_zaman = 5000  # saniye
 
-oran = 1.e10  # Bu oran sistemi ekrana sığdırmak için kullanılacak
+oran = 3.e9  # Bu oran sistemi ekrana sığdırmak için kullanılacak
+cap_oran = 0.25e1  # Bu oran cisimlerin çaplarını ekrana sığdırmak için kullanılacak
 
 arkaplan_rengi = (20, 20, 20)
 (en, boy) = (1000, 1000)
@@ -81,24 +83,25 @@ class Gokcismi:
         self.koordinatlar[0] += self.hiz[0] * ayrik_zaman
         self.koordinatlar[1] += self.hiz[1] * ayrik_zaman
 
-    def tum_cisimlere_kuvvet_etki():
-        kopya_cisimler = list(cisimler)
-        for i in range(len(cisimler)):
-            cisimler[i].hizi_degistir(kopya_cisimler)
+def tum_cisimlere_kuvvet_etki(cisimler):
+    kopya_cisimler = list(cisimler)
+    for i in range(len(cisimler)):
+        cisimler[i].hizi_degistir(kopya_cisimler)
 
-    def tum_cisimleri_hareket_ettir():
-        for i in range(len(cisimler)):
-            cisimler[i].hizi_uygula()
+def tum_cisimleri_hareket_ettir(cisimler):
+    for i in range(len(cisimler)):
+        cisimler[i].hizi_uygula()
 
-    def tum_cisimleri_cizdir():
-        for i in range(len(cisimler)):
-            yeni_koordinatlar = (cisimler[i].koordinatlar[
-                                 0] / oran, cisimler[i].koordinatlar[1] / oran)
-            yeni_koordinatlar = (
-                int(yeni_koordinatlar[0] + en / 2), int(boy / 2 - yeni_koordinatlar[1]))
-            yeni_cap = int(cisimler[i].cap / oran)
-            pygame.draw.circle(
-                ekran, cisimler[i].renk, yeni_koordinatlar, yeni_cap)
+def tum_cisimleri_cizdir(cisimler, referans_cisim):
+    for i in range(len(cisimler)):
+
+        yeni_koordinatlar = (cisimler[i].koordinatlar[0] - referans_cisim.koordinatlar[0],
+                                cisimler[i].koordinatlar[1] - referans_cisim.koordinatlar[1])
+        yeni_koordinatlar = (yeni_koordinatlar[0] / oran, yeni_koordinatlar[1] / oran)
+        yeni_koordinatlar = (int(yeni_koordinatlar[0] + en / 2), int(boy / 2 - yeni_koordinatlar[1]))
+        yeni_cap = int(math.log(cisimler[i].cap) / cap_oran)
+        pygame.draw.circle(
+            ekran, cisimler[i].renk, yeni_koordinatlar, yeni_cap)
 
 dunya = Gokcismi('Dunya', dunyanin_kutlesi, dunyanin_capi,
                  dunya_gunes_uzaklik, (0, 0, 255))
@@ -115,15 +118,18 @@ ay = Gokcismi('Ay', ayin_kutlesi, ayin_capi, ay_gunes_uzaklik, (80, 80, 80))
 saturn = Gokcismi('Saturn', saturnun_kutlesi, saturnun_capi,
                   saturn_gunes_uzaklik, (230, 200, 200))
 
-cisimler += [gunes, dunya, jupiter, mars, venus, merkur, saturn]
+#karadelik = Gokcismi('Karadelik', gunesin_kutlesi * 1, merkurun_capi, dunya_gunes_uzaklik * 1.2, (255, 255, 255))
+
+cisimler += [gunes, dunya, jupiter, mars, venus, merkur]#, saturn]#, karadelik]
 
 running = True
 while running:
-    Gokcismi.tum_cisimleri_hareket_ettir()
-    Gokcismi.tum_cisimlere_kuvvet_etki()
-    Gokcismi.tum_cisimleri_cizdir()
-    pygame.display.update()
+    tum_cisimleri_hareket_ettir(cisimler)
+    tum_cisimlere_kuvvet_etki(cisimler)
+
     ekran.fill(arkaplan_rengi)
+    tum_cisimleri_cizdir(cisimler, gunes) # Baska gezegenlere göre çizdirmek için referans cismi değiştir!
+    pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
